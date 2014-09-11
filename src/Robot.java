eaaa6c7c002c96e94ebc14773976e6a805e7da23
Ophir LOJKINE <pere.jobs@gmail.com>
2014-09-11 09:32:49 +0200

    Add getters, change variable names, fix error

--- src/Robot.java
+++ src/Robot.java
@@ -3,14 +3,14 @@
  */
 public class Robot {
 	private String name;
-	private Point2D pos;
+	private Point2D position;
 	private int energy;
 	private int health;
 
 	/** Crée un robot */
 	public Robot(String name) {
 		this.name = name;
-		this.pos = new Point2D(0,0);
+		this.position = new Point2D(0,0);
 		this.energy = 100;
 		this.health = 100;
 	}
@@ -22,11 +22,14 @@ public class Robot {
 	public void setEnergy(int energy) {this.energy = energy;}	
 
 	/** @param position new position*/
-	public void setPosition(int position) {this.position = position;}	
+	public void setPosition(Point2D position) {this.position = position;}	
 
 	/** @return The level of health of the robot*/
 	public int getHealth() {return this.health;}	
 
 	/** @return the energy of the robot*/
-	public int getHealth() {return this.health;}	
+	public int getEnergy() {return this.energy;}	
+
+	/** @return the position of the robot*/
+	public Point2D getPosition() {return this.position;}	
 }
