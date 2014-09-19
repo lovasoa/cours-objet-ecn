8d4126244dad1833b63de0bd9c6a21d1f3a00523
ophir <ophir.lojkine@af83.com>
2014-09-19 16:54:56 +0200

    Change main method message

--- src/sirop/SIROP.java
+++ src/sirop/SIROP.java
@@ -11,7 +11,7 @@ package sirop;
  */
 public class SIROP {
     public static void main(String[] args){
-    System.out.println("hello world");
+    System.out.println("Welcome to SIROP!");
     }
     
 }
