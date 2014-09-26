8bb8f6d1a107c66bc3b076b55b7dcce88c1bf76f
ophir <ophir.lojkine@af83.com>
2014-09-26 15:15:57 +0200

    Travail sur le plateau de jeu

--- src/sirop/PlateauJeu.java
+++ src/sirop/PlateauJeu.java
@@ -1,6 +1,8 @@
 package sirop;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * @author lovasoa
@@ -18,6 +20,9 @@ public class PlateauJeu {
   public PlateauJeu(int largeur, int hauteur) {
     this.dimensions[0] = largeur;
     this.dimensions[1] = hauteur;
+    robots = new ArrayList<>(0);
+    obstacles = new ArrayList<>(0);
+    bonuses = new ArrayList<>(0);
   }
   
   public void ajouterRobot(Robot robot) {
@@ -25,6 +30,29 @@ public class PlateauJeu {
   }
   
   public void tourDeJeu() {
-    
+    boolean continuer = true;
+    Scanner scanner = new Scanner(System.in);
+
+    while (continuer){
+      
+      System.out.println("Nouveau tour de jeu:");
+      System.out.println(" robots (" + robots.size() +"): ");
+      for (Robot robot : this.robots) {
+        System.out.println("- " + robot);
+      }
+
+      System.out.println(" obstacles (" + obstacles.size() +"): ");
+      for (Obstacle obstacle : this.obstacles) {
+        System.out.println("- " + obstacle);
+      }
+      
+      System.out.println(" bonuses (" + bonuses.size() +"): ");
+      for (Bonus bonus : this.bonuses) {
+        System.out.println("- " + bonus);
+      }
+      
+      System.out.println("Voulez-vous continuer? (oui/non) ");
+      continuer = scanner.nextLine().trim().equalsIgnoreCase("oui");
+    }
   }
 }
--- src/sirop/RobotEnergivore.java
+++ src/sirop/RobotEnergivore.java
@@ -6,7 +6,6 @@ package sirop;
  * @author zhaoshuli
  */
 public class RobotEnergivore extends Robot{
-        
         public RobotEnergivore(String name){
             super(name);
         }
