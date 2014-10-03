8c0f9a8e787b4bb8de56780a9d465ee5907f2748
zhao111 <zhaoshuli@ext-130-66-207-55-wifi.ec-nantes.fr>
2014-10-03 12:01:37 +0200

    BonusMobile

--- src/sirop/BonusMobile.java
+++ src/sirop/BonusMobile.java
@@ -8,5 +8,19 @@ package sirop;
  * @author zhaoshuli
  */
 public class BonusMobile extends Bonus{
+    /**Create a mobile bonus*/
+    public BonusMobile(Point2D PositionMobile, int pointsSante, int pointsEnergie, PlateauJeu plateau){
+        super(PositionMobile, pointsSante, pointsEnergie, plateau);
+    }
+    
+    /**@return the type of the bonus*/
+        public String getType (){
+            return "BonuslMobile.";}
+        
+        public boolean deplacer(Point2D vect){
+              this.getPosition().deplacer(vect);
+              return true;
+        }
+        
     
 }
